acda77b579bc9a368e50eeb045d33c1045f3811c
Caroline LS <carolinelds@gmail.com>
2022-04-25 06:23:01 -0300

    Exibe todos os feriados do ano com a requisição em /holydays

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(cors());
 
 app.get("/holidays", (request,response) => {
+    const holidays = [
+        { date: "1/1/2022", name: "Confraternização mundial" },
+        { date: "1/3/2022", name: "Carnaval" },
+        { date: "4/17/2022", name: "Páscoa" },
+        { date: "4/21/2022", name: "Tiradentes" },
+        { date: "5/1/2022", name: "Dia do trabalho" },
+        { date: "6/16/2022", name: "Corpus Christi" },
+        { date: "9/7/2022", name: "Independência do Brasil" },
+        { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+        { date: "11/2/2022", name: "Finados" },
+        { date: "11/15/2022", name: "Proclamação da República" },
+        { date: "12/25/2022", name: "Natal" }
+      ];
 
+    let holydaysBrasil = [];
+
+    holidays.map(holiday => {
+          let dataBrasil = dayjs(holiday.date).format("DD/MM/YYYY");
+          holydaysBrasil.push(`${dataBrasil}: ${holiday.name}`);
+      })
+    
+    holydaysBrasil = ["Lista de feriados em 2022: ", ...holydaysBrasil];
+    holydaysBrasil = holydaysBrasil.join("<br />");
+
+    response.send(holydaysBrasil);
 })
 
 app.get("/is-today-holiday", (request,response) => {
